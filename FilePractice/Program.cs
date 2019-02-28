2f3ab38e6c0fb3f14fc9094f6e543bae0ef4e9cb
Машимов Наурызбай Кенесариевич <@corp.itstep>
2019-02-28 21:36:46 +0600

    File

---
+++ FilePractice/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace FilePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> stat = new Dictionary<char, int>();
+
+            Console.WriteLine("Введите путь к файлу:");
+            string path = Console.ReadLine();
+            using (FileStream stream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                byte[] bytes = new byte[stream.Length];
+
+                stream.Read(bytes, 0, bytes.Length);
+
+                string data = System.Text.Encoding.UTF8.GetString(bytes);
+                foreach (char symbol in data)
+                {
+                    if (stat.ContainsKey(symbol))
+                    {
+                        stat[symbol]++;
+                    }
+                    else
+                    {
+                        stat[symbol] = 0;
+                    }
+                }
+                Console.WriteLine(data);
+            }
+            foreach (KeyValuePair<char, int> item in stat)
+            {
+                Console.WriteLine(item.Key + "\t\t" + item.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IOLesson/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("IOLesson")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IOLesson")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("d9e9caba-7815-4d1c-a93c-bd2779b362d6")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
